c52b82db3b36eaec4ad353df116d57295aec0c48
Olga Tsiouri <olgatsiouri@outlook.com>
2020-11-03 23:06:04 +0200

    Update fasta_length_calculator.jl

--- biomisc_julia/sequence_statistics/fasta_length_calculator.jl
+++ biomisc_julia/sequence_statistics/fasta_length_calculator.jl
@@ -1,7 +1,7 @@
 # julia
 using FASTX
 using BioSequences
-print("calculate the length of a multi or single fasta file and outputs in a tabular format\nusage: julia aa_content_calculator.jl input_fasta output_txt")
+print("calculate the length of a multi or single fasta file and outputs in a tabular format\nusage: julia fasta_length_calculator.jl input_fasta output_txt")
 # main
 reader = open(FASTA.Reader,ARGS[1])
 open(ARGS[2],"a") do io
