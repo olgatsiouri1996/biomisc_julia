78098077af4842e83d44987be25c6c580d3faf0c
Olga Tsiouri <olgatsiouri@outlook.com>
2020-11-04 23:20:22 +0200

    Add files via upload

---
+++ biomisc_julia/sequence_statistics/select_fasta_by_cys.jl
@@ -0,0 +1,19 @@
+# julia
+using FASTX
+using BioSequences
+# create function for gc content calculation
+function cys_content(seq)
+    comp = composition(seq)
+    return round(comp[AA_C] * 100 / length(seq), digits = 2)
+end
+# main
+print("calculate the %cysteine content of a multi or single fasta file and output a fasta format file with a user specified percentage threshold\nusage: julia select_header_by_cys.jl input_fasta min_cyc max_cys output_fasta")
+reader = open(FASTA.Reader,ARGS[1])
+open(ARGS[4],"a") do io
+   for record in reader
+        if parse(Int, ARGS[2]) < cys_content(FASTA.sequence(record)) < parse(Int, ARGS[3])
+            println(io, record)
+        end
+    end
+    close(reader)
+end
