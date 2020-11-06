00385bec6d7bb7edb58b666a863be5c40cef69b9
Olga Tsiouri <olgatsiouri@outlook.com>
2020-11-06 14:01:11 +0200

    Add files via upload

---
+++ biomisc_julia/argparse_equivalents/sequence_statistics/proteins/select_header_by_aa.jl
@@ -0,0 +1,44 @@
+# julia
+using FASTX
+using BioSequences
+using ArgParse
+# input parameters function
+function parse_commandline()
+
+    s = ArgParseSettings(description = "calculate %aa content from fasta and multifasta and output a tabular file with ids and description using user specified thresholds")
+    @add_arg_table s begin
+        "--in"
+            help = "input fasta file"
+        "--aa"
+            help = "amino acid to search the content for"
+        "--min"
+            help = "min aa% content"
+        "--max"
+            help = "max aa% content"
+        "--out"
+            help = "output txt file"
+    end
+    return parse_args(s)
+end
+
+function main()
+    parsed_args = parse_commandline()
+    println(parsed_args)
+    # create function for aa content calculation
+    function aa_content(seq)
+        comp = composition(seq)
+        return round(comp[convert(AminoAcid, first(collect(parsed_args["aa"])))] * 100 / length(seq), digits = 2)
+    end
+    # main
+    reader = open(FASTA.Reader, parsed_args["in"])
+    open(parsed_args["out"],"a") do io
+        for record in reader
+            if parse(Int, parsed_args["min"]) < aa_content(FASTA.sequence(record)) < parse(Int, parsed_args["max"])
+                println(io,FASTA.identifier(record),"\t",FASTA.description(record))
+            end
+        end
+    end
+    close(reader)
+end
+
+main()
