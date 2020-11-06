15b653349a68bc3616e855140b8f2ea111c6fddf
Olga Tsiouri <olgatsiouri@outlook.com>
2020-11-06 02:34:57 +0200

    Update fasta_to_ids.jl

--- biomisc_julia/fasta_manipulation/fasta_to_ids.jl
+++ biomisc_julia/fasta_manipulation/fasta_to_ids.jl
@@ -5,7 +5,7 @@ print("usage: julia fasta_to_ids.jl input_fasta output_txt")
 reader = open(FASTA.Reader,ARGS[1])
 open(ARGS[2],"a") do io
    for record in reader
-    println(io,FASTA.identifier(record))
+        println(io,FASTA.identifier(record))
     end
 end
 close(reader)
