b689e28e39a0145bb26790b106c4d99f1d3160b4
Olga Tsiouri <olgatsiouri@outlook.com>
2020-11-06 02:48:16 +0200

    Add files via upload

--- biomisc_julia/fasta_manipulation/fasta_to_ids.jl
+++ biomisc_julia/fasta_manipulation/fasta_to_ids.jl
@@ -4,8 +4,8 @@ using FASTX
 print("usage: julia fasta_to_ids.jl input_fasta output_txt")
 reader = open(FASTA.Reader,ARGS[1])
 open(ARGS[2],"a") do io
-   for record in reader
-        println(io,FASTA.identifier(record))
-    end
+for record in reader
+    println(io,FASTA.identifier(record))
 end
 close(reader)
+end
