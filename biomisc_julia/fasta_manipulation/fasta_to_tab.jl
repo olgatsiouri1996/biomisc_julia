3ef600f80c6fe3ac115660f6c7a4a13cb5a6871f
Olga Tsiouri <olgatsiouri@outlook.com>
2020-11-06 03:24:08 +0200

    Update fasta_to_tab.jl

--- biomisc_julia/fasta_manipulation/fasta_to_tab.jl
+++ biomisc_julia/fasta_manipulation/fasta_to_tab.jl
@@ -4,8 +4,8 @@ using FASTX
 print("convert the fasta file into a tablular format file\nusage: julia fasta_to_tab.jl input_fasta output_txt")
 reader = open(FASTA.Reader,ARGS[1])
 open(ARGS[2],"a") do io
-for record in reader
-    println(io,FASTA.identifier(record),"\t",FASTA.sequence(record))
-end
+    for record in reader
+        println(io,FASTA.identifier(record),"\t",FASTA.sequence(record))
+    end
 close(reader)
 end
