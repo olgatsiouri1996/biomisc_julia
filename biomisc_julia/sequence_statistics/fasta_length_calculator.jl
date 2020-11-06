defad67b539a857ee896528a601915ce1a12abf3
Olga Tsiouri <olgatsiouri@outlook.com>
2020-11-06 03:26:29 +0200

    Update fasta_length_calculator.jl

--- biomisc_julia/sequence_statistics/fasta_length_calculator.jl
+++ biomisc_julia/sequence_statistics/fasta_length_calculator.jl
@@ -6,7 +6,7 @@ print("calculate the length of a multi or single fasta file and outputs in a tab
 reader = open(FASTA.Reader,ARGS[1])
 open(ARGS[2],"a") do io
    for record in reader
-    println(io,FASTA.identifier(record),"\t",length(FASTA.sequence(record)))
-    end
+      println(io,FASTA.identifier(record),"\t",length(FASTA.sequence(record)))
+   end
 end
 close(reader)
