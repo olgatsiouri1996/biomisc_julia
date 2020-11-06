34ad4f905f6ff33a2408f767ba1281add080a61d
Olga Tsiouri <olgatsiouri@outlook.com>
2020-11-06 03:22:28 +0200

    Update fasta_subset_by_length.jl

--- biomisc_julia/fasta_manipulation/fasta_subset_by_length.jl
+++ biomisc_julia/fasta_manipulation/fasta_subset_by_length.jl
@@ -4,10 +4,10 @@ using FASTX
 print("collect proteins with user specified length \nusage: julia fasta_collect_putative_effectome.jl input_fasta min_length max_length output_fasta")
 reader = open(FASTA.Reader,ARGS[1])
 open(ARGS[4],"a") do io
-for record in reader
-    if parse(Int, ARGS[2]) < length(FASTA.sequence(record)) < parse(Int, ARGS[3])
-        println(io,record)
+    for record in reader
+        if parse(Int, ARGS[2]) < length(FASTA.sequence(record)) < parse(Int, ARGS[3])
+            println(io,record)
+        end
     end
-end
 close(reader)
 end
