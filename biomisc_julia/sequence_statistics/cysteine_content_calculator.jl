cd7dacc962afb7b4c45bae01f1dfb632202d1cf9
Olga Tsiouri <olgatsiouri@outlook.com>
2020-11-06 03:25:55 +0200

    Update cysteine_content_calculator.jl

--- biomisc_julia/sequence_statistics/cysteine_content_calculator.jl
+++ biomisc_julia/sequence_statistics/cysteine_content_calculator.jl
@@ -10,8 +10,8 @@ end
 print("calculate the %cysteine content of a multi or single fasta file and outputs in a tabular format\nusage: julia cysteine_content_calculator.jl input_fasta output_txt")
 reader = open(FASTA.Reader,ARGS[1])
 open(ARGS[2],"a") do io
-   for record in reader
-    println(io,FASTA.identifier(record),"\t",cys_content(FASTA.sequence(record)))
+    for record in reader
+        println(io,FASTA.identifier(record),"\t",cys_content(FASTA.sequence(record)))
     end
 end
 close(reader)
