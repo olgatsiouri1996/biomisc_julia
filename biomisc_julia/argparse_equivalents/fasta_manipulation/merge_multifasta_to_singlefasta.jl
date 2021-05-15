19b7b04c1d68b9a299f3bf7a9dd0d24d992bb40f
Olga Tsiouri <olgatsiouri@outlook.com>
2021-05-15 03:15:44 +0300

    Add files via upload

---
+++ biomisc_julia/argparse_equivalents/fasta_manipulation/merge_multifasta_to_singlefasta.jl
@@ -0,0 +1,35 @@
+# julia
+using ArgParse
+using FASTX
+# input parameters function
+function parse_commandline()
+
+    s = ArgParseSettings(description = "merge sequences on a multifasta file to a single fasta")
+    @add_arg_table s begin
+        "--in"
+            help = "input fasta file"
+        "--header"
+            help = "output fasta header"
+        "--out"
+            help = "output fasta file"
+    end
+    return parse_args(s)
+end
+
+function main()
+    parsed_args = parse_commandline()
+    println(parsed_args)
+    open(parsed_args["out"],"a") do io
+        println(io,">",parsed_args["header"])
+    end
+    reader = open(FASTA.Reader, parsed_args["in"])
+    open(parsed_args["out"],"a") do io
+        for record in reader
+            merged_seqs = join(FASTA.sequence(record))
+        print(io,merged_seqs)
+        end
+    end
+    close(reader)
+end
+
+main()
