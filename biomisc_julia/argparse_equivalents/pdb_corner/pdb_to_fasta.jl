25e1d453ad0272670ee45dc668ff95661a9a8db6
Olga Tsiouri <olgatsiouri@outlook.com>
2022-04-26 00:42:58 +0300

    fasta format corrected, file inserted from directory instead of downloading

--- biomisc_julia/argparse_equivalents/pdb_corner/pdb_to_fasta.jl
+++ biomisc_julia/argparse_equivalents/pdb_corner/pdb_to_fasta.jl
@@ -1,17 +1,20 @@
 # julia
 using ArgParse
 using BioStructures
+using FASTX
 # input parameters function
 function parse_commandline()
 
-    s = ArgParseSettings(description = "read a pdb file from PDB and convert it to fasta without the need to download it")
+    s = ArgParseSettings(description = "read a pdb file from the current directory and convert it to fasta without the need to download it")
     @add_arg_table s begin
         "--pdb"
-            help = "input pdb ids"
+            help = "input pdb file name of a pdb file from the same directory"
+            required = true
         "--model"
-            help = "model to make the fasta file for"
-        "--out"
-            help = "output fasta file"
+            help = "model to make the fasta file for. Type 1 if you have only 1 model. Default 1"
+            arg_type = Int
+            default = 1
+            required = false
     end
     return parse_args(s)
 end
@@ -20,11 +23,12 @@ function main()
     parsed_args = parse_commandline()
     println(parsed_args)
 # main
-    downloadpdb(parsed_args["pdb"]) do fp
-        s = read(fp, PDB)
-        open(parsed_args["out"],"a") do io
-            println(io,">",parsed_args["pdb"],"\n",LongAminoAcidSeq(s[parse(Int, parsed_args["model"])], standardselector))
-        end
+    s = read(join([parsed_args["pdb"],".pdb"], ""), PDB)
+    seq = String(LongAminoAcidSeq(s[parsed_args["model"]], standardselector, gaps=false))
+    id = String(parsed_args["pdb"])
+    rec = FASTA.Record(id,seq)
+    open(FASTA.Writer,join([id,".fasta"],""), width=60) do w
+        write(w,rec)
     end
 end
 
