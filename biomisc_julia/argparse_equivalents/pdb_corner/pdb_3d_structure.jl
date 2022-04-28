7e6f24091daf96b17b56e6b4b00af85fa41df88a
Olga Tsiouri <olgatsiouri@outlook.com>
2022-04-28 19:10:43 +0300

    the script contains the option to select between a file from the current directory or a pdb file to be downloaded from PDB

--- biomisc_julia/argparse_equivalents/pdb_corner/pdb_3d_structure.jl
+++ biomisc_julia/argparse_equivalents/pdb_corner/pdb_3d_structure.jl
@@ -9,12 +9,18 @@ function parse_commandline()
     s = ArgParseSettings(description = "prints the 3D structure of a pdb file, by selecting the model")
     @add_arg_table s begin
         "--pdb"
-            help = "input pdb file"
+            help = "input pdb file's id"
+            required = true
         "--model"
-            help = "model to select in the pdb file(default=1)"
+            help = "model to select in the pdb file"
             arg_type = Int
             default = 1
-            required = false   
+            required = false
+        "--mode"
+            help = "select either to inport a pdb file from the directory or to download from PDB. default is to download from PDB"
+            arg_type = Int
+            default = 1
+            required = false    
     end
     return parse_args(s)
 end
@@ -22,8 +28,16 @@ end
 function main()
     parsed_args = parse_commandline()
     println(parsed_args)
-	struc = read(parsed_args["pdb"], PDB)
-	viewstruc(struc[parsed_args["model"]])	
+# Choose program
+    if parsed_args["mode"]==1
+        downloadpdb(parsed_args["pdb"]) do fp
+        struc = read(fp, PDB)
+        viewstruc(struc[parsed_args["model"]])
+    end
+    else
+    	struc = read(join([parsed_args["pdb"],".pdb"],""), PDB)
+    	viewstruc(struc[parsed_args["model"]])
+    end	
 end
 
 main()	 
