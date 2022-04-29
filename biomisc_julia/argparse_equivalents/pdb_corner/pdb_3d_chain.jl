17cd9a6247b5ac8f368bed022f55b202e8e46635
Olga Tsiouri <olgatsiouri@outlook.com>
2022-04-29 04:44:30 +0300

    the script contains the option to select between a file from the current directory or a pdb file to be downloaded from PDB

--- biomisc_julia/argparse_equivalents/pdb_corner/pdb_3d_chain.jl
+++ biomisc_julia/argparse_equivalents/pdb_corner/pdb_3d_chain.jl
@@ -10,13 +10,22 @@ function parse_commandline()
     @add_arg_table s begin
         "--pdb"
             help = "input pdb file"
+            required = true
         "--model"
             help = "model to select in the pdb file(default=1)"
             arg_type = Int
             default = 1
             required = false
        	"--chain"
-            help = "chain to select in the pdb file"    
+            help = "chain to select in the pdb file"
+            arg_type = String
+            default = "A"
+            required = false
+        "--mode"
+            help = "select either to inport a pdb file from the directory or to download from PDB. default is to download from PDB"
+            arg_type = Int
+            default = 1
+            required = false    
     end
     return parse_args(s)
 end
@@ -24,8 +33,16 @@ end
 function main()
     parsed_args = parse_commandline()
     println(parsed_args)
-	struc = read(parsed_args["pdb"], PDB)
-	viewstruc(struc[parsed_args["model"]][parsed_args["chain"]])	
+    # Choose program
+    if parsed_args["mode"]==1
+        downloadpdb(parsed_args["pdb"]) do fp
+        struc = read(fp, PDB)
+        viewstruc(struc[parsed_args["model"]][parsed_args["chain"]])
+    end
+    else
+        struc = read(join([parsed_args["pdb"],".pdb"],""), PDB)
+        viewstruc(struc[parsed_args["model"]][parsed_args["chain"]])
+    end 
 end
- 
+    
 main()	
