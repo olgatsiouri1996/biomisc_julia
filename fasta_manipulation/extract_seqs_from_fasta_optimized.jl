4f893e93949e848a5d08d2e6798f484e766dd327
Olga Tsiouri <olgatsiouri@outlook.com>
2022-05-15 04:23:10 +0300

    Create extract_seqs_from_fasta_optimized.jl

---
+++ fasta_manipulation/extract_seqs_from_fasta_optimized.jl
@@ -0,0 +1,68 @@
+# julia
+using ArgParse
+using FASTX
+using DelimitedFiles
+# input parameters function
+function parse_commandline()
+
+    s = ArgParseSettings(description = "extract fasta records from a multi-fasta file by using a txt file with fasta identifiers with an input index fai file for bigger speed")
+    @add_arg_table s begin
+        "--in"
+            help = "input multi-fasta file"
+            required = true
+        "--fai"
+            help = "input .fai index file(created using samtools faidx)"
+            required = true
+        "--txt"
+            help = "input 1-column txt file with ids"
+            required = true
+        "--out"
+            help = "output multi-fasta file"
+            required = false
+        "--type"
+            help = "type of fasta files to output 1. multi-fasta 2. single-fasta"
+            arg_type = Int
+            default = 1
+            required = false
+        "--dir"
+            help = "directory to save the single-fasta files(relative or absolute path)"
+            default = "."
+            required = false
+    end
+    return parse_args(s)
+end
+
+function main()
+    parsed_args = parse_commandline()
+    println(parsed_args)
+    # main
+    # import txt file
+    ids = readdlm(parsed_args["txt"], '\t', String, '\n',quotes=false)
+    # import multi-fasta
+    reader = open(FASTA.Reader, parsed_args["in"], index= parsed_args["fai"])
+    # choose output type
+    if parsed_args["type"]==1
+        open(FASTA.Writer,parsed_args["out"], width=60, append=true) do w
+            for id in ids
+                record = reader[String(id)]
+                write(w,record)
+            end
+        end
+        close(reader)
+
+    else
+        # select directory to write single-fasta files
+        cd(parsed_args["dir"])
+        # split each record to a seperate fasta file
+        for id in ids
+            record = reader[String(id)]
+            open(FASTA.Writer,join([FASTA.identifier(record),".fasta"],""), width=60) do w
+                write(w,record)
+            end
+        end
+        close(reader)
+    end
+
+end
+
+main()
